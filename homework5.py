ce5df33345fb50aaa61e57f7546fce06c3b115a1
AliyaKayumova <aliya-kayumova@inbox.ru>
2024-06-16 12:55:54 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+immutable_var = 1, 2, 'a', 'b'
+print(immutable_var)
+
+mutable_list = [1, 2, 'a', 'b']
+print(mutable_list)
+mutable_list [0] = 5
+print(mutable_list)
+
+immutable_var[0]= 5   # пренесла ниже, чтобы остальной код работал
+print(immutable_var) # измнение не возможно, так как объекты "tuple" не поддерживает изменение элемнтов
