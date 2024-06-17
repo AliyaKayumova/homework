48af24a8bf4bcb626297e7fc25048d5f176a006f
AliyaKayumova <aliya-kayumova@inbox.ru>
2024-06-17 23:48:25 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,16 @@
+my_dict = {'Aliya': 2001, 'Ivan': 1997, 'Mary': 2003}
+print(my_dict)
+print(my_dict['Aliya'])
+print(my_dict.get('Niki'))
+my_dict.update({'Anna': 1999, 'Misha': 2005})
+a = my_dict.pop('Mary')
+print(a)
+print(my_dict)
+
+
+my_set = {1 , 'Rain' , 3.14 , 1 , 3.14}
+print(my_set)
+my_set.add('Sun')
+my_set.add(60)
+my_set.remove(1)
+print(my_set)
